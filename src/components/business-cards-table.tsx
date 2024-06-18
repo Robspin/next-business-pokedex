9624e8e46a00e0f84284c16a18589133b56bd947
Robspin <robin_steeman@hotmail.com>
2024-06-18 21:23:26 +0800

    added favicon

--- src/components/business-cards-table.tsx
+++ src/components/business-cards-table.tsx
@@ -117,10 +117,10 @@ export default function BusinessCardsTable({ businessCards }: Props) {
         <div className="w-full">
             <div className="flex items-center py-4">
                 <Input
-                    placeholder="Filter emails..."
-                    value={(table.getColumn("email")?.getFilterValue() as string) ?? ""}
+                    placeholder="Filter by name..."
+                    value={(table.getColumn("name")?.getFilterValue() as string) ?? ""}
                     onChange={(event) =>
-                        table.getColumn("email")?.setFilterValue(event.target.value)
+                        table.getColumn("name")?.setFilterValue(event.target.value)
                     }
                     className="w-[200px]"
                 />
